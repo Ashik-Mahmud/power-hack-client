ab91cc8ce92ae83304c4ec202f89b17dd5102f34
ashikmahmud <ashikmamud187@gmail.com>
2022-06-30 20:28:32 +0600

    init Power Hack client and putting some important Documentations

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    content: [
+      "./src/**/*.{js,jsx,ts,tsx}",
+    ],
+    theme: {
+      fontFamily: {
+            poppins: ['poppins', 'sans-serif'],
+            raleway: ['Raleway', 'sans-serif'],
+          },
+      extend: {},
+    },
+    plugins: [],
+}
